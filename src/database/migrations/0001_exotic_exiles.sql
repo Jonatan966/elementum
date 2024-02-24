fb14eb96885aae647e0ecde11718da36cda0bb25
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-02-24 17:53:16 -0300

    feat: add index to elements table columns

---
+++ src/database/migrations/0001_exotic_exiles.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS "element_a_idx" ON "combinations" ("element_a");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "element_b_idx" ON "combinations" ("element_b");
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,15 +1,22 @@
 import { i18n } from "@/app/i18n/settings";
 import { sql } from "drizzle-orm";
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { index, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const combinations = pgTable("combinations", {
-  id: uuid("id")
-    .primaryKey()
-    .default(sql`gen_random_uuid()`),
-  elementA: varchar("element_a").notNull(),
-  elementB: varchar("element_b").notNull(),
-  result: varchar("result").notNull(),
-  emoji: varchar("emoji").notNull(),
-  language: varchar("language", { enum: i18n.locales }).notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
+export const combinations = pgTable(
+  "combinations",
+  {
+    id: uuid("id")
+      .primaryKey()
+      .default(sql`gen_random_uuid()`),
+    elementA: varchar("element_a").notNull(),
+    elementB: varchar("element_b").notNull(),
+    result: varchar("result").notNull(),
+    emoji: varchar("emoji").notNull(),
+    language: varchar("language", { enum: i18n.locales }).notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+  },
+  (table) => ({
+    elementAIdx: index("element_a_idx").on(table.elementA),
+    elementBIdx: index("element_b_idx").on(table.elementB),
+  })
+);
