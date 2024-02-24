bd0f7661484182a02ac1919ab0dd11c099000e78
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-02-24 18:04:03 -0300

    feat: add spanish translate

--- src/app/i18n/index.ts
+++ src/app/i18n/index.ts
@@ -5,6 +5,7 @@ import type { Locale } from "./settings";
 // We also get the default import for cleaner types
 const dictionaries = {
   en: () => import("./locales/en.json").then((module) => module.default),
+  es: () => import("./locales/es.json").then((module) => module.default),
   "pt-BR": () =>
     import("./locales/pt-BR.json").then((module) => module.default),
 };
